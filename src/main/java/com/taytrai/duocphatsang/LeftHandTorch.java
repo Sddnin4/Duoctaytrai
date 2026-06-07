2e2d1bce8af3d09b1c3fde28cddc230ba7f20c9b
Mila <builonggiaduc08032010@gmail.com>
2026-06-07 19:06:47 +0700

    Modify LeftHandTorch to register TorchListener with this

--- src/main/java/com/taytrai/duocphatsang/LeftHandTorch.java
+++ src/main/java/com/taytrai/duocphatsang/LeftHandTorch.java
@@ -1,18 +1,37 @@
 package com.taytrai.duocphatsang;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LeftHandTorch extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Đăng ký sự kiện lắng nghe người chơi cầm đuốc
-        getServer().getPluginManager().registerEvents(new TorchListener(), this);
+        // Đã truyền 'this' vào đây để kích hoạt bộ hẹn giờ
+        getServer().getPluginManager().registerEvents(new TorchListener(this), this);
         getLogger().info("Plugin Cam Duoc Tay Trai phat sang da hoat dong tren 1.21!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Plugin da dung!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("phattuoc")) {
+            if (sender instanceof Player) {
+                Player p = (Player) sender;
+                ItemStack torch = new ItemStack(Material.TORCH);
+                p.getInventory().setItemInOffHand(torch);
+                p.sendMessage("Đã ép đuốc vào tay trái!");
+                return true;
+            }
+        }
+        return false;
     }
 }
